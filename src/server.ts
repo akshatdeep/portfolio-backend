39acfdf1e196f56e55afbf8d0af0957ddf055a29
Suhani Gupta <suhani@Suhanis-MacBook-Air.local>
2025-12-12 20:08:08 +0530

    vercel cores

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 // Create Socket.io server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL ,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
@@ -72,7 +72,7 @@ io.on("connection", (socket) => {
 });
 
 // Server listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
